c550b0b2fa0436d8c339a32f8e87e7412aae5ad5
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-04 23:15:44 +0200

    completed part 1 of inside PrintTo5

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,13 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        while (i<6)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+        System.out.println("Done");
+
         // Complete the program's code here:
     }
  }
