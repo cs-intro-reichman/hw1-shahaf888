8f6b74ce5d396f3c1885342fb7f3979652996953
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-05 00:03:08 +0200

    complete first try Bill3

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		//we can assume that the inputs are valid - 3 names (as strings) and then number (as integer) if the bill
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		String amount = args[3];
+		int argument = Integer.parseInt(amount);
+		double avg=argument/3.0;
+		System.out.println("Dear "+name1+", "+name2+", "+name3+": pay " +Math.ceil(avg)+ " each");
+
 	}
 }
