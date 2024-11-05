b71f874a7b8bd47747df05b09ef1ab38a2f0b334
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-05 14:32:27 +0200

    i have finished ascend

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,22 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		String input=args[0];
+		int range=Integer.parseInt(input);
+		int a = (int) (Math.random() * range);
+		int b = (int) (Math.random() * range);
+		int c = (int) (Math.random() * range);
+		//int max=Math.max(a, b);
+		//max=Math.max(max,c);
+		int max= Math.max(a, Math.max(b,c));
+		int min= Math.min(a, Math.min(b,c));
+		//int min = Math.min(a,b);
+		//min=Math.min(min,c);
+		int middle = a + b + c - max - min;
+
+		System.out.println(a + " " + b + " " + c);
+		System.out.println(min + " "+ middle +" " + max);
+
+		System.out.println(c);
 	}
 }
