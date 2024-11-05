d40e6b121c6b0ac35746ec0da7a2278eeb1acdab
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-05 13:56:05 +0200

    i have finished numwords

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,15 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		String number = args[0];
+		int int_number= Integer.parseInt(number);
+		int ones=int_number%10;
+		int_number=int_number/10;
+		int tens=int_number%10;
+		int_number=int_number/10;
+		int hundreds=int_number%10;
+		System.out.println( hundreds +" hundreds, "+ tens + " tens, " + ones + " ones.");
+
+
 	}
 }
