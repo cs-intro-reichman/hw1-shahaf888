c0ee86cbc804eb16d0fcfde9ea0088fe960edd18
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-06 16:07:46 +0200

    FVCalc after changing

--- FVCalc.java
+++ FVCalc.java
@@ -9,7 +9,7 @@ public class FVCalc {
 		int new_currentValue=Integer.parseInt(currentValue);
 		double sum_after=(new_currentValue*Math.pow(1.0+(new_rate/100),new_nYears));
 		int sum_after_convert= (int) sum_after;
-		System.out.println("after "+nYears+" years $"+currentValue+" saved at "+new_rate+"% will yield $"+ sum_after_convert);
+		System.out.println("After "+nYears+" years, a $"+currentValue+" saved at "+new_rate+"% will yield $"+ sum_after_convert);
 
 
 
