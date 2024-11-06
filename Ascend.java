3069165c1a0833d294e28a3f658b00113b613a86
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-06 16:14:44 +0200

    I submit again

--- Ascend.java
+++ Ascend.java
@@ -7,17 +7,12 @@ public class Ascend {
 		int a = (int) (Math.random() * range);
 		int b = (int) (Math.random() * range);
 		int c = (int) (Math.random() * range);
-		//int max=Math.max(a, b);
-		//max=Math.max(max,c);
+
 		int max= Math.max(a, Math.max(b,c));
 		int min= Math.min(a, Math.min(b,c));
-		//int min = Math.min(a,b);
-		//min=Math.min(min,c);
 		int middle = a + b + c - max - min;
 
 		System.out.println(a + " " + b + " " + c);
 		System.out.println(min + " "+ middle +" " + max);
-
-		System.out.println(c);
 	}
 }
