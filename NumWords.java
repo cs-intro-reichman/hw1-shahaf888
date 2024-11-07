84ed2dec1db07fe7dd5443ff7a57f574a28c55ae
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-07 17:11:34 +0200

    after we put abs for negative input

--- NumWords.java
+++ NumWords.java
@@ -1,9 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    // taking input from the command line and parse it to integer
 		String number = args[0];
 		int int_number= Integer.parseInt(number);
+		//if we have negative input
+		int_number=Math.abs(int_number);
+		//split the number with %10 and /10
 		int ones=int_number%10;
 		int_number=int_number/10;
 		int tens=int_number%10;
